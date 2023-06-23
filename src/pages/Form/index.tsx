cc5094f28d80375d3a8a3314f0c0401850b4a25d
ldcss <ldcs@cin.ufpe.br>
2023-06-23 00:39:33 -0300

    <feat> finish create task form

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,22 +1,121 @@
-import { Paper } from '@mui/material';
-import { FormContainer } from './style';
+/* eslint-disable react/jsx-no-bind */
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from '@mui/material';
+import { FormComponent, Container } from './style';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { CreateTaskDto } from '../../types/Task';
+import { TaskService } from '../../services/TaskService';
+import { ToastContainer, toast } from 'react-toastify';
+
+const initialTask: CreateTaskDto = {
+  name: '',
+  description: '',
+  priority: '',
+  member: '',
+};
 
 function Form() {
+  const [task, setTask] = useState<CreateTaskDto>(initialTask);
+
+  function updateTaskData(event: ChangeEvent<any> | SelectChangeEvent<string>) {
+    return setTask({ ...task, [event.target.name]: event.target.value });
+  }
+
+  async function postTask() {
+    TaskService.postTask(task)
+      .then(() => toast.success('Task criada com sucesso!'))
+      .catch(() => toast.error('Task não foi criada. Tente novamente!'));
+  }
+
+  function submit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('task', task);
+    postTask();
+  }
   return (
-    <Paper
-      sx={{
-        height: '90vh',
-        width: '100vw',
-        backgroundColor: '#FFFFFF',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        flexDirection: 'column',
-      }}
-    >
-      <h1>CADASTRO DE TASKS</h1>
-      <FormContainer></FormContainer>
-    </Paper>
+    <Container>
+      <ToastContainer position='top-right' />
+      <h1>Cadastro de task</h1>
+      <FormComponent onSubmit={submit}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            gap: '1rem',
+          }}
+        >
+          <TextField
+            name='name'
+            label='Nome'
+            type='text'
+            required
+            onChange={updateTaskData}
+            style={{ width: '80%', marginTop: '20px' }}
+            value={task.name}
+          />
+          <TextField
+            name='description'
+            label='Descrição'
+            type='text'
+            required={false}
+            onChange={updateTaskData}
+            style={{ width: '80%' }}
+            value={task.description}
+            multiline
+          />
+          <FormControl style={{ width: '80%' }}>
+            <InputLabel>Prioridade</InputLabel>
+            <Select
+              name='priority'
+              label='Prioridade'
+              onChange={updateTaskData}
+              required={false}
+              value={task.priority}
+            >
+              <MenuItem value='Baixa'>Baixa</MenuItem>
+              <MenuItem value='Media'>Média</MenuItem>
+              <MenuItem value='Alta'>Alta</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl style={{ width: '80%' }}>
+            <InputLabel>Membro</InputLabel>
+            <Select
+              name='member'
+              label='Membro'
+              onChange={updateTaskData}
+              required={false}
+              value={task.member}
+            >
+              <MenuItem value='Membros'>Membros</MenuItem>
+            </Select>
+          </FormControl>
+          <Button
+            variant='contained'
+            type='submit'
+            style={{
+              position: 'relative',
+              height: '3em',
+              width: '100%',
+              maxWidth: '8em',
+              marginBottom: '20px',
+              marginLeft: 'auto',
+              marginRight: '20px',
+            }}
+          >
+            Salvar
+          </Button>
+        </div>
+      </FormComponent>
+    </Container>
   );
 }
 
--- src/pages/Form/style.ts
+++ src/pages/Form/style.ts
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 import Box from '@mui/material/Box/Box';
 
-export const FormContainer = styled(Box)({
-  height: '50vh',
-  width: '50vw',
+export const Container = styled(Box)({
+  height: '90vh',
+  width: '100vw',
+  backgroundColor: '#FFFFFF',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  flexDirection: 'column',
+});
+
+export const FormComponent = styled('form')({
+  minHeight: '50vh',
+  minWidth: '50vw',
   backgroundColor: '#E6E6FA',
   borderRadius: '20px',
   border: '2px solid black;',
